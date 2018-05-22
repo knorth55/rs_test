d8c6f7977b8ec9b0214ec06b3df4159031c0ee97
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-05-22 17:25:43 +0900

    add python node using boost::python

--- src/PythonTestAnnotator.cpp
+++ src/PythonTestAnnotator.cpp
@@ -1,26 +1,27 @@
+#include <boost/python.hpp>
 #include <uima/api.hpp>
 
 #include <pcl/point_types.h>
 #include <rs/types/all_types.h>
-
 //RS
 #include <rs/scene_cas.h>
 #include <rs/utils/time.h>
 
-#include <pcl/common/centroid.h>
-
+namespace python = boost::python;
 using namespace uima;
 
 
-class MyFirstAnnotator : public Annotator
+class PythonTestAnnotator : public Annotator
 {
 private:
+  float test_param;
 
 public:
 
   TyErrorId initialize(AnnotatorContext &ctx)
   {
     outInfo("initialize");
+    ctx.extractValue("test_param", test_param);
     return UIMA_ERR_NONE;
   }
 
@@ -36,14 +37,18 @@ public:
     rs::StopWatch clock;
     rs::SceneCas cas(tcas);
     pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZRGBA>);
-    outInfo("test param = " << test_param);
-    cas.get(VIEW_CLOUD, *cloud_ptr);
+    outInfo("Test param =  " << test_param);
+    cas.get(VIEW_CLOUD,*cloud_ptr);
 
     outInfo("Cloud size: " << cloud_ptr->points.size());
+    python::object math_module = python::import("math");
+    python::object math_sqrt = math_module.attr("sqrt");
+    float res = python::extract<float>(math_sqrt(cloud_ptr->points.size())); 
+    outInfo("Sqrt cloud size by Python: " << res);
     outInfo("took: " << clock.getTime() << " ms.");
     return UIMA_ERR_NONE;
   }
 };
 
 // This macro exports an entry point that is used to create the annotator.
-MAKE_AE(MyFirstAnnotator)
+MAKE_AE(PythonTestAnnotator)
