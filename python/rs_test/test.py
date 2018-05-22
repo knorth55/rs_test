550304aef72aeb928e5dd990401ed5e116920138
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-05-22 20:07:52 +0900

    add boost-numpy

--- python/rs_test/test.py
+++ python/rs_test/test.py
@@ -1,5 +1,10 @@
+import numpy as np
 import random
 
 
 def random_add(n):
     return n + random.randint(1, 100)
+
+
+def random_add_array(arr):
+    return arr + np.random.uniform(0.0, 1.0, size=arr.size)
